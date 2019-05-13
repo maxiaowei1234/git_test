2f958e5778bdd8ac59521e489226dd635afe3d1a
mazai <2692316729@qq.com>
2019-05-13 16:50:21 +0800

    吧本地工程添加到git

---
+++ src/com/itheima/git/git_Text.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class git_Text {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
